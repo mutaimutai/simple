9bbffee3471f95958d29261a5da7945e3bf905e7
emmanuelnjihia <125468645+emmanuel-njihia@users.noreply.github.com>
2023-07-26 13:57:28 +0300

    Update my_getLine.c

--- my_getLine.c
+++ my_getLine.c
@@ -124,7 +124,7 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
  * Return: s
  */
 
-int _getline(info_t *info, char **ptr, size_t *length)
+int my_getline(info_t *info, char **ptr, size_t *length)
 
 {
 	static char buf[READ_BUF_SIZE];
