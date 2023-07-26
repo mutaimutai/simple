efa5d6e67c42c358073f81bca8e53d0cad9e8070
emmanuelnjihia <125468645+emmanuel-njihia@users.noreply.github.com>
2023-07-26 13:57:15 +0300

    Update and rename getLine.c to my_getLine.c

--- my_getLine.c
+++ my_getLine.c
@@ -116,7 +116,7 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 }
 
 /**
- * _getline - line input for STDIN
+ * my_getline - line input for STDIN
  * @info: struct parameter
  * @ptr: address of pointer to buffer, preallocated or NULL
  * @length: size of preallocated ptr buffer if not NULL
